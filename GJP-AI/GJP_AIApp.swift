746278e0d74bdc5a92f6b6e589b8d493a2122c5c
Johnny Gan <ganjpaien@gmail.com>
2026-05-08 07:52:44 +0800

    feat: add initial iOS app scaffold

---
+++ GJP-AI/GJP_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GJP_AIApp.swift
+//  GJP-AI
+//
+//  Created by GJP AI on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct GJP_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
